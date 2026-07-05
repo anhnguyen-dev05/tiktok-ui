92e08ce5aa8dde033786c1ede9c811de38b9189f
DucAnh <nguyenduyducanh19102005@gmail.com>
2026-07-05 13:58:43 +0700

    feat: add search and input bar of header

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+import logo from '@/assets/tiktok.svg';
+
+export default {
+  logo,
+};
--- src/layouts/components/Header/index.jsx
+++ src/layouts/components/Header/index.jsx
@@ -1,11 +1,33 @@
-import clsx from 'clsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faCircleNotch, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 import styles from './Header.module.scss';
+import images from '@/assets';
 
 function Header() {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.inner}></div>
-    </div>
+    <header className={styles.wrapper}>
+      <div className={styles.inner}>
+        <div className={styles.logo}>
+          <img src={images.logo} alt="Tiktok"></img>
+          <strong>TikTok</strong>
+        </div>
+
+        <div className={styles.search}>
+          <input placeholder="Search accounts and videos" />
+          <button className={styles.clear}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          {/* <FontAwesomeIcon className={styles.loading} icon={faCircleNotch} spin spinReverse /> */}
+
+          <button className={styles.searchBtn}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+
+        <div className={styles.actions}></div>
+      </div>
+    </header>
   );
 }
 
