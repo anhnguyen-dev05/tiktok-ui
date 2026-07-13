f6c0a1bf333f3b14f1af8b484700880c01c939f8
DucAnh <nguyenduyducanh19102005@gmail.com>
2026-07-13 15:29:28 +0700

    refactor: move route paths to config/routes.js and add link for logo Tiktok

--- src/layouts/components/Header/index.jsx
+++ src/layouts/components/Header/index.jsx
@@ -11,6 +11,7 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
+import { Link } from 'react-router-dom';
 
 import Button from '@/components/Button';
 import styles from './Header.module.scss';
@@ -19,6 +20,7 @@ import Menu from '@/components/Menu';
 import Image from '@/components/Image';
 import { UploadIcon, MessageIcon, InboxIcon } from '@/components/Icons';
 import Search from '../Search';
+import routesConfig from '@/config/routes';
 
 const MENU_ITEMS = [
   {
@@ -88,7 +90,9 @@ function Header() {
     <header className={styles.wrapper}>
       <div className={styles.inner}>
         <div className={styles.logo}>
-          <img src={logo} alt="TikTok" />
+          <Link to={routesConfig.home} className={styles.logoLink}>
+            <img src={logo} alt="TikTok" />
+          </Link>
         </div>
 
         <Search />
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,5 @@
+import routesConfig from '@/config/routes';
+
 //layouts
 import { HeaderOnly } from '@/layouts';
 
@@ -9,11 +11,11 @@ import Search from '@/layouts/components/Search';
 
 // public routes
 const publicRoutes = [
-  { path: '/', component: Home },
-  { path: '/following', component: Following },
-  { path: '/profile/:nickname', component: Profile },
-  { path: '/upload', component: Upload, layout: HeaderOnly },
-  { path: '/search', component: Search, layout: null },
+  { path: routesConfig.home, component: Home },
+  { path: routesConfig.following, component: Following },
+  { path: routesConfig.profile, component: Profile },
+  { path: routesConfig.upload, component: Upload, layout: HeaderOnly },
+  { path: routesConfig.search, component: Search, layout: null },
 ];
 
 const privateRoutes = [];
